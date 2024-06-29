f18b3e42a9af16601163743a3e8e0477b46ec289
Gustavo F. Balbino <gustavofbdeveloper@gmail.com>
2024-06-28 22:46:27 -0300

    Atualização dos preços

--- api/src/controllers/PriceReadjustments.controller.ts
+++ api/src/controllers/PriceReadjustments.controller.ts
@@ -2,9 +2,11 @@ import { Request, Response } from "express";
 import DBConnectionPool from "../config/database";
 import ValidateCSVData from "../use-cases/ValidateCSVData";
 import { ValidateCSVDataCommand } from "../use-cases/ValidateCSVData.command";
+import UpdatePricesCommand from "../use-cases/UpdatePrices.command";
+import UpdatePrices from "../use-cases/UpdatePrices";
 
 export default class PriceReadjustmentController {
-  constructor(private validateCsvData: ValidateCSVData) {}
+  constructor(private validateCsvData: ValidateCSVData, private updatePrices: UpdatePrices) {}
 
   async validate(request: Request, response: Response) {
     const command = request.body as ValidateCSVDataCommand;
@@ -12,5 +14,13 @@ export default class PriceReadjustmentController {
     const result = await this.validateCsvData.execute(command);
 
     return response.status(200).json(result);
+  }
+
+  async update(request: Request, response: Response) {
+    const command = request.body as UpdatePricesCommand;
+
+    await this.updatePrices.execute(command);
+
+    return response.status(204).send();
   }
 }
--- api/src/router.ts
+++ api/src/router.ts
@@ -7,4 +7,8 @@ router.route("/validatecsvdata").post((req, res) => {
   return priceReadjustmentController.validate(req, res);
 });
 
+router.route("/updateprices").post((req, res) => {
+  return priceReadjustmentController.update(req, res);
+});
+
 export default router;
